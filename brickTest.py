2600378f1ec8b7690f93412cede10059ea757f0b
Silas Monahan <monahans@carleton.edu>
2021-04-07 14:20:44 -0500

    better brickTest.py

--- brickTest.py
+++ brickTest.py
@@ -12,10 +12,22 @@ g.bind("boliou", BOLIOU)
 BRICK = Namespace("https://brickschema.org/schema/Brick#")
 g.bind("brick", BRICK)
 
+
+g.add((BOLIOU["Boliou"], RDF.type, BRICK.Building))
+
+g.add((BOLIOU["Ground-Floor"], RDF.type, BRICK.Floor))
+g.add((BOLIOU["Boliou"], BRICK.hasPart, BOLIOU["Ground-Floor"]))
+g.add((BOLIOU["First-Floor"], RDF.type, BRICK.Floor))
+g.add((BOLIOU["Boliou"], BRICK.hasPart, BOLIOU["First-Floor"]))
+
 g.add((BOLIOU["Room-021"], RDF.type, BRICK.Room))
+g.add((BOLIOU["Ground-Floor"], BRICK.hasPart, BOLIOU["Room-021"]))
 g.add((BOLIOU["Room-022"], RDF.type, BRICK.Room))
-g.add((BOLIOU["Room-055"], RDF.type, BRICK.Room))
-g.add((BOLIOU["Room-059"], RDF.type, BRICK.Room))
+g.add((BOLIOU["Ground-Floor"], BRICK.hasPart, BOLIOU["Room-022"]))
+g.add((BOLIOU["Room-155"], RDF.type, BRICK.Room))
+g.add((BOLIOU["First-Floor"], BRICK.hasPart, BOLIOU["Room-155"]))
+g.add((BOLIOU["Room-159"], RDF.type, BRICK.Room))
+g.add((BOLIOU["First-Floor"], BRICK.hasPart, BOLIOU["Room-159"]))
 
 g.add((BOLIOU["021-Room_Temp_Setpoint"], RDF.type, BRICK.Room_Air_Temperature_Setpoint))
 g.add((BOLIOU["Room-021"], BRICK.hasPoint, BOLIOU["021-Room_Temp_Setpoint"]))
@@ -26,7 +38,15 @@ g.add((BOLIOU["Room-021"], BRICK.hasPoint, BOLIOU["021-Room_Temp_Setpoint"]))
 EVANS = Namespace("http://example.com/evans#")
 g.bind("evans", EVANS)
 
+g.add((EVANS["Evans"], RDF.type, BRICK.Building))
+
+g.add((EVANS["Ground-Floor"], RDF.type, BRICK.Floor))
+g.add((EVANS["Evans"], BRICK.hasPart, EVANS["Ground-Floor"]))
+g.add((EVANS["First-Floor"], RDF.type, BRICK.Floor))
+g.add((EVANS["Evans"], BRICK.hasPart, EVANS["First-Floor"]))
+
 g.add((EVANS["Room-120"], RDF.type, BRICK.Room))
+g.add((EVANS["First-Floor"], BRICK.hasPart, EVANS["Room-120"]))
 g.add((EVANS["120-Room_Temp_Setpoint"], RDF.type, BRICK.Room_Air_Temperature_Setpoint))
 g.add((EVANS["Room-120"], BRICK.hasPoint, EVANS["120-Room_Temp_Setpoint"]))
 g.add((EVANS["120-Room_Temp"], RDF.type, BRICK.Room_Air_Temperature))
@@ -35,20 +55,18 @@ g.add((EVANS["Room-120"], BRICK.hasPoint, EVANS["120-Room_Temp"]))
 
 
 g.add((EVANS["Room-122"], RDF.type, BRICK.Room))
+g.add((EVANS["First-Floor"], BRICK.hasPart, EVANS["Room-122"]))
 g.add((EVANS["122-Room_Temp_Setpoint"], RDF.type, BRICK.Room_Air_Temperature_Setpoint))
 g.add((EVANS["Room-122"], BRICK.hasPoint, EVANS["122-Room_Temp_Setpoint"]))
 g.add((EVANS["122-Room_Temp"], RDF.type, BRICK.Room_Air_Temperature))
 g.add((EVANS["Room-122"], BRICK.hasPoint, EVANS["122-Room_Temp"]))
 
 g.add((EVANS["Room-G05"], RDF.type, BRICK.Room))
+g.add((EVANS["Ground-Floor"], BRICK.hasPart, EVANS["Room-G05"]))
 g.add((EVANS["Room-G16"], RDF.type, BRICK.Room))
+g.add((EVANS["Ground-Floor"], BRICK.hasPart, EVANS["Room-G16"]))
 
 
 with open("example.ttl", "wb") as f:
     # the Turtle format strikes a balance beteween being compact and easy to read
     f.write(g.serialize(format="ttl"))
-
-
-
-
-
